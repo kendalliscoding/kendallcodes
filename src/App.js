b0b6688cd2e66d210db3fabb1e28c8d61c6cea39
Kendall Holmes <98422018+kendallatada@users.noreply.github.com>
2023-01-27 17:01:46 -0800

    Add about me page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home.js";
+import Home from "./pages/Home";
+import AboutMe from "./pages/AboutMe";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/aboutme" element={<AboutMe />} />
     </Routes>
   );
 }
