11b310091a377018a7b9c5614d5ca591206d8d52
Kendall Holmes <98422018+kendallatada@users.noreply.github.com>
2023-01-31 12:41:13 -0800

    create about me page skeleton

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -1,9 +1,18 @@
 import NavBar from "../components/NavBar";
+import "./AboutMe.css";
+import AboutMeEntry from "../components/AboutMeEntry";
+import aboutMeEntries from "../data/aboutme-test.json";
 
 const AboutMe = () => {
+  const entries = aboutMeEntries.map((entry) => {
+    return <AboutMeEntry title={entry.title} desc={entry.description} />;
+  });
   return (
     <div>
       <NavBar />
+      <main>
+        <div className="entries">{entries}</div>
+      </main>
     </div>
   );
 };
