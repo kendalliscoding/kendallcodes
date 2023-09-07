b0103ef69f975e614620b6d58f9e4ed586ee606c
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2023-09-06 23:38:34 -0700

    concept for project reveal

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,7 +1,17 @@
 import './Projects.css';
+import { useState } from 'react';
 
 const Projects = () => {
-    return <section className="projects">hey</section>
+    const [revealScreen, setRevealScreen] = useState(true);
+    const revealId = revealScreen ? "reveal-screen-on" : "reveal-screen-off";
+    return (
+        <section className="projects">
+            <div id={revealId} onClick={() => setRevealScreen(!revealScreen)}>Click Me</div>
+            <img id="homepage-screenshot" alt="insert homepage screenshot here" />
+            <p id="website-details"> This website! 🎉 Check out the source code ** here **  </p>
+            
+        </section>
+    );
 }
 
 export default Projects;
