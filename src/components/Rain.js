34c9dd4e7697df0952c48a236fd9862b59513f5d
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2023-10-09 16:03:43 -0700

    reset rain when tab focus changes

--- src/components/Rain.js
+++ src/components/Rain.js
@@ -4,7 +4,7 @@ import { useState } from "react";
 
 const Rain = () => {
     const [raindrops, setRaindrops] = useState([]);
-
+    
     const createRaindrops = (queuePosition) => {
         setRaindrops([...raindrops, <Raindrop key={queuePosition} queuePosition={queuePosition} />])
     }
@@ -12,11 +12,17 @@ const Rain = () => {
         setTimeout(() => createRaindrops(raindrops.length + 1), 300);
     }
 
+    // reset rain when tab is not active
     const resetRain = () => {
-        setRaindrops([]);
+        if (document.visibilityState === "visible") {
+            console.log("resetting rain");
+            setRaindrops([]);
+          }
     }
+    document.addEventListener("visibilitychange", resetRain);
+
     return (
-        <div className="rain" onFocus={resetRain}>
+        <div className="rain">
             {raindrops}
         </div>
     )
