ea865eafc9766e60ce9a1d908f495fc28d9dda4b
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2023-10-11 14:47:37 -0700

    add website icon and styling

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,7 @@
 import './Projects.css';
 import { useState } from 'react';
 import confetti from 'canvas-confetti';
+import websiteIcon from '../photos/website-icon.png';
 
 const Projects = () => {
     const [hideContent, setHideContent] = useState(true);
@@ -11,11 +12,7 @@ const Projects = () => {
     const revealProject = (e) => {
         confetti({
             particleCount: 200,
-            spread: 100,
-            // origin: { // why wont you woooork 😭
-            //     x: e.clientX / window.screen.width,
-            //     y: e.clientY / window.screen.height
-            // }
+            spread: 100
         });
         setHideContent(!hideContent);
     }
@@ -24,11 +21,11 @@ const Projects = () => {
         <section className="projects">
             <div id={contentScreenId} onClick={(e) => revealProject(e)}>reveal</div>
             <div className={`projects-content ${contentBlur}`}>
-                <img id="homepage-screenshot" alt="insert homepage screenshot here" src="https://placekitten.com/200/300"/>
+                <img id="homepage-screenshot" alt="Computer icon displaying text that says 'Kendall Codes'" src={websiteIcon} />
                 <div id="website-details"> 
-                    <p>This website! 🎉</p>
-                    <p>Building this portfolio website with React has been an opportunity for me to dive deeper into web development and highlight my accomplishments. Thank you for visiting. 😌☀️✨ </p>
-                    <p>Check out the source code <a id="project-source-code" href="https://github.com/kendalliscoding/kendallcodes" target="_blank">here</a>  </p> 
+                    <p>This website!</p>
+                    <p>Building this portfolio website with React has been an opportunity for me to dive deeper into web development and highlight my accomplishments. Thank you for visiting. :)</p>
+                    <p>Check out the source code <a id="project-source-code" href="https://github.com/kendalliscoding/kendallcodes" target="_blank">here</a>~  </p> 
                 </div>
             </div>
             
