cff67d66534a6316c664d3fe85a792b265d6b768
kendall holmes <121322044+kendalliscoding@users.noreply.github.com>
2023-10-11 16:57:57 -0700

    add github and linkedin links to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import "./Footer.css";
+import linkedinLogo from "../photos/LI-In-Bug.png"
+import githubLogo from "../photos/github-mark-white.png";
+
 const Footer = () => {
   return (
     <footer>
-      <div className="github-link" />
-      <div className="linkedin-link" />
+      <a href="https://github.com/kendalliscoding">
+        <img id="github-link" src={githubLogo} alt="github logo" />
+      </a>
+      <a href="https://www.linkedin.com/in/kendallholmes/">
+        <img id="linkedin-link" src={linkedinLogo} alt="linkedin logo" />
+      </a>
       <div className="copyright">&copy; Kendall Holmes</div>
     </footer>
   );
